ac58b586ca74bb6bc807e995ac221ce7a5f159cc
Wei-Jie Huang <weii41392@gmail.com>
2023-10-09 00:42:16 -0500

    host static files w/ flask

--- flask_container/app/__init__.py
+++ flask_container/app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='../build')
 CORS(app)
 from . import views
