be249ff5ef596fe6814cef05283ebce5b376c4fc
collebrusco <collebrusco.frank@gmail.com>
2023-10-16 13:46:01 -0500

    get stable integrated containers working. Backend has code that tests the DB access, which works. For now, DB will add some default data on startup. Frontend is served on 3000

---
+++ init-mongo.js
@@ -0,0 +1,33 @@
+// init-mongo.js
+db.auth('admin', 'adminpassword')
+db = db.getSiblingDB('mydatabase');
+db.createUser({
+  user: 'myuser',
+  pwd: 'mypassword',
+  roles: [{
+    role:'root',
+    db:'mydatabase'
+  }]
+});
+
+// Insert data into the 'projects' collection
+db.projects.insertOne({
+  "_id": ObjectId("651a62a3b5248dbcc86fbef1"),
+  "title": "Sample Project",
+  "description": "Description for Sample Project",
+  "resources": [
+    {
+      "resource_id": ObjectId("651a5a8bb5248dbcc8638dbe"),
+      "quantity": 3
+    },
+    {
+      "resource_id": ObjectId("651a5a8bb5248dbcc8638dc0"),
+      "quantity": 2
+    }
+  ],
+  "user_ids": [ObjectId("65235a9bdf748fa11b4724cd")],
+  "creator_id": ObjectId("651a5a3fb5248dbcc8631798")
+});
+
+
+print('Database initialized successfully.');
