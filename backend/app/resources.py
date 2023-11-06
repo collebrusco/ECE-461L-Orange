c857e3ea6de8ce11ac06cd8f6377a150d712d602
Spencer Shis <--get>
2023-11-06 12:59:50 -0600

    integration hell

--- backend/app/resources.py
+++ backend/app/resources.py
@@ -15,92 +15,68 @@ def all_resources():
 
     return Response(status=200, response=resources, content_type="application/json")
 
-@app.route('resources/{resource_id}/checkout', methods='[POST]')
+@app.route('resources/{resource_title}/checkout', methods='[POST]')
 @require_jwt
-def checkout(resource_id):
-    
-    # if resource_id NULL or not a string
-    if not resource_id:
-        return Response(status=400, response="Malformed request")
-    
-    # Get our amount and project from query
-    amount = request.args.get('amount')
-    pid = request.args.get('project_id')
-    # Verify We got Amount
-    if amount is None:
-        return Response(status=400, response="Malformed Request: No amount provided")
-    if pid is None:
-        return Response(status=400, response="Malformed Request: No project id provided")
-    # Get our resource
-    resources = client["resources"]
-    resource = resources.find_one({"id": resource_id})
-
-    if resource is None:
-        return Response(status=404, response="Resource not found")
-    
-    # Get our project
-    projects = client["projects"]
-    project = projects.find_one({"id": pid})
-
-    if project is None:
-        return Response(status=404, response="Project not found")
-    
-    
-    if resource["availability"] < amount:
-        return Response(status=400, response="Tried to check out too many")
-    else:
-        resource["availability"] = resource["availability"] - amount
-        client["resources"].update_one({"id": resource_id}, resource)
-
-    checked = project["resources"][resource_id]
-    checked += amount
-    ins = {resource_id: checked}
-    project["resources"].update(ins)
-    client["projects"].update_one({"id":pid}, project)
-    
-    return Response(status=200, response=jsonify(resource), content_type="application/json")
-
-@app.route('resources/{resource_id}/checkin', methods='[POST]')
-@require_jwt
-def checkin(resource_id):
-    # TODO: Get user from jwt and put our resource into their project
-    # if resource_id NULL or not a string
-    if not resource_id:
+def checkout(resource_title):
+    # TODO: Get project from request body and update it
+    # if resource_title NULL or not a string
+    if not resource_title:
         return Response(status=400, response="Malformed request")
     
     # Get our amount from query
     amount = request.args.get('amount')
-    pid = request.args.get('project_id')
+    
     # Verify We got Amount
     if amount is None:
         return Response(status=400, response="Malformed Request: No amount provided")
-    if pid is None:
-        return Response(status=400, response="Malformed Request: No project id provided")
+    
     # Get our resource
     resources = client["resources"]
-    resource = resources.find_one({"id": resource_id})
+    resource = resources.find_one({"title": resource_title})
 
-     # Get our project
-    projects = client["projects"]
-    project = projects.find_one({"id": pid})
-
-    if project is None:
-        return Response(status=404, response="Project not found")
-    
-
+    # Check to see if we have specified resource
     if resource is None:
         return Response(status=404, response="Resource not found")
     
+    # If we have enough available to check out 
+    if resource["availability"] < amount:
+        return Response(status=400, response="Tried to check out too many")
+    else:
+        resource["availability"] = resource["availability"] + amount
+        # Update Resource in DB
+        client["resources"].update_one({"title": resource_title}, resource)
+    
+    return Response(status=200, response=jsonify(resource), content_type="application/json")
+
+@app.route('resources/{resource_title}/checkin', methods='[POST]')
+@require_jwt
+def checkin(resource_title):
+    # TODO: Get project from request body and update it
+    # if resource_title NULL or not a string
+    if not resource_title:
+        return Response(status=400, response="Malformed request")
+    
+    # Get our amount from query
+    amount = request.args.get('amount')
+    
+    # Verify We got Amount
+    if amount is None:
+        return Response(status=400, response="Malformed Request: No amount provided")
+    
+    # Get our resource
+    resources = client["resources"]
+    resource = resources.find_one({"title": resource_title})
+
+    # Check to see if we have specified resource
+    if resource is None:
+        return Response(status=404, response="Resource not found")
+    
+    # If we are not exceeding capacity TODO: Make this if we are not exceeding the amount specified project has checked in
     if resource["capacity"] < amount + resource["availability"]:
         return Response(status=400, response="Tried to check in too many")
     else:
         resource["availability"] = resource["availability"] + amount
-        client["resources"].update_one({"id": resource_id}, resource)
-
-    checked = project["resources"][resource_id]
-    checked -= amount
-    ins = {resource_id: checked}
-    project["resources"].update(ins)
-    client["projects"].update_one({"id":pid}, project)
+        # Update Resource in DB
+        client["resources"].update_one({"title": resource_title}, resource)
     
     return Response(status=200, response=jsonify(resource), content_type="application/json")
