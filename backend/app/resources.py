e4b3f8887cd9c2e6e52b33e777dbbd241a67ece3
Spencer Shisler <spencershisler@utexas.edu>
2023-11-06 11:25:17 -0600

    project update

--- backend/app/resources.py
+++ backend/app/resources.py
@@ -18,18 +18,19 @@ def all_resources():
 @app.route('resources/{resource_id}/checkout', methods='[POST]')
 @require_jwt
 def checkout(resource_id):
-    # TODO: Get user from jwt and put our resource into their project
+    
     # if resource_id NULL or not a string
     if not resource_id:
         return Response(status=400, response="Malformed request")
     
-    # Get our amount from query
+    # Get our amount and project from query
     amount = request.args.get('amount')
-    
+    pid = request.args.get('project_id')
     # Verify We got Amount
     if amount is None:
         return Response(status=400, response="Malformed Request: No amount provided")
-    
+    if pid is None:
+        return Response(status=400, response="Malformed Request: No project id provided")
     # Get our resource
     resources = client["resources"]
     resource = resources.find_one({"id": resource_id})
@@ -37,11 +38,25 @@ def checkout(resource_id):
     if resource is None:
         return Response(status=404, response="Resource not found")
     
+    # Get our project
+    projects = client["projects"]
+    project = projects.find_one({"id": pid})
+
+    if project is None:
+        return Response(status=404, response="Project not found")
+    
+    
     if resource["availability"] < amount:
         return Response(status=400, response="Tried to check out too many")
     else:
         resource["availability"] = resource["availability"] - amount
         client["resources"].update_one({"id": resource_id}, resource)
+
+    checked = project["resources"][resource_id]
+    checked += amount
+    ins = {resource_id: checked}
+    project["resources"].update(ins)
+    client["projects"].update_one({"id":pid}, project)
     
     return Response(status=200, response=jsonify(resource), content_type="application/json")
 
@@ -55,14 +70,23 @@ def checkin(resource_id):
     
     # Get our amount from query
     amount = request.args.get('amount')
-    
+    pid = request.args.get('project_id')
     # Verify We got Amount
     if amount is None:
         return Response(status=400, response="Malformed Request: No amount provided")
-    
+    if pid is None:
+        return Response(status=400, response="Malformed Request: No project id provided")
     # Get our resource
     resources = client["resources"]
     resource = resources.find_one({"id": resource_id})
+
+     # Get our project
+    projects = client["projects"]
+    project = projects.find_one({"id": pid})
+
+    if project is None:
+        return Response(status=404, response="Project not found")
+    
 
     if resource is None:
         return Response(status=404, response="Resource not found")
@@ -72,5 +96,11 @@ def checkin(resource_id):
     else:
         resource["availability"] = resource["availability"] + amount
         client["resources"].update_one({"id": resource_id}, resource)
+
+    checked = project["resources"][resource_id]
+    checked -= amount
+    ins = {resource_id: checked}
+    project["resources"].update(ins)
+    client["projects"].update_one({"id":pid}, project)
     
     return Response(status=200, response=jsonify(resource), content_type="application/json")
